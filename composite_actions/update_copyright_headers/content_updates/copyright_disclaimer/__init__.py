590595705bfb3c81dcd5d7b00db568351ff68469
OSrivalli <36564011+OSrivalli@users.noreply.github.com>
2024-10-27 22:33:57 +0530

    Update __init__.py

---
+++ composite_actions/update_copyright_headers/content_updates/copyright_disclaimer/__init__.py
@@ -0,0 +1,6 @@
+"""
+Update or add a new disclaimer to the input lines
+"""
+
+from content_updates.copyright_disclaimer.insert_disclaimer import get_disclaimer_text
+from content_updates.copyright_disclaimer.transform_disclaimer import process_disclaimer
